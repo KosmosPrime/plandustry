df1fbf55bfa43dc24c2a4d90cecc586761a35e8a
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2023-01-02 23:14:16 +0100

    Implement base block registration

---
+++ src/block/base.rs
@@ -0,0 +1,20 @@
+use crate::block::make_register;
+use crate::block::simple::SimpleBlock;
+
+make_register!
+(
+	MENDER: "mender" => SimpleBlock::new(1, true);
+	MEND_PROJECTOR: "mend-projector" => SimpleBlock::new(2, true);
+	OVERDRIVE_PROJECTOR: "overdrive-projector" => SimpleBlock::new(2, true);
+	OVERDRIVE_DOME: "overdrive-dome" => SimpleBlock::new(3, true);
+	FORCE_PROJECTOR: "force-projector" => SimpleBlock::new(3, true);
+	SHOCK_MINE: "shock-mine" => SimpleBlock::new(1, true);
+	CORE_SHARD: "core-shard" => SimpleBlock::new(3, true);
+	CORE_FOUNDATION: "core-foundation" => SimpleBlock::new(4, true);
+	CORE_NUCLEUS: "core-nucleus" => SimpleBlock::new(5, true);
+	CONTAINER: "container" => SimpleBlock::new(2, true);
+	VAULT: "vault" => SimpleBlock::new(3, true);
+	UNLOADER: "unloader" => SimpleBlock::new(1, true); // TODO config: item
+	ILLUMINATOR: "illuminator" => SimpleBlock::new(1, true); // TODO config: color
+	LAUNCH_PAD: "launch-pad" => SimpleBlock::new(3, true);
+);
