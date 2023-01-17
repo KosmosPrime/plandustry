f68d6bb61ecc6a9542a9eb2dfdc8efc21084c16c
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2023-01-17 22:14:40 +0100

    Add an item content representation

---
+++ src/item/mod.rs
@@ -0,0 +1,30 @@
+use crate::content::content_enum;
+
+content_enum!
+{
+	pub enum Type / Item for u16 | TryFromU16Error
+	{
+		Copper => "copper",
+		Lead => "lead",
+		Metaglass => "metaglass",
+		Graphite => "graphite",
+		Sand => "sand",
+		Coal => "coal",
+		Titanium => "titanium",
+		Thorium => "thorium",
+		Scrap => "scrap",
+		Silicon => "silicon",
+		Plastanium => "plastanium",
+		PhaseFabric => "phase-fabric",
+		SurgeAlloy => "surge-alloy",
+		SporePod => "spore-pod",
+		BlastCompound => "blast-compound",
+		Pyratite => "pyratite",
+		Beryllium => "beryllium",
+		Tungsten => "tungsten",
+		Oxide => "oxide",
+		Carbide => "carbide",
+		FissileMatter => "fissile-matter",
+		DormantCyst => "dormant-cyst",
+	}
+}
