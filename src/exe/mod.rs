806f71cb9422c231fde8aa49dbc5d3d08d6a7229
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2023-02-12 21:27:06 +0100

    Make invalid command error print not panic

--- src/exe/mod.rs
+++ src/exe/mod.rs
@@ -33,9 +33,9 @@ pub fn main(mut args: Args)
 {
 	match args.next()
 	{
-		None => panic!("not enough arguments"),
+		None => eprintln!("Not enough arguments, valid commands are: edit, print"),
 		Some(s) if s == "edit" => edit::main(args, 1),
 		Some(s) if s == "print" => print::main(args, 1),
-		Some(s) => panic!("unknown argument {s}"),
+		Some(s) => eprintln!("Unknown argument {s}, valid commands are: edit, print"),
 	}
 }
