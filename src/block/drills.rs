4cf98855348a1a27a21833196c0b8ed2c29f44f9
bendn <bend.n@outlook.com>
2023-07-26 19:22:51 +0700

    correctly classify vent condenser

--- src/block/drills.rs
+++ src/block/drills.rs
@@ -1,6 +1,7 @@
 //! extraction of raw resources (mine part)
 use crate::block::simple::{cost, make_simple};
 use crate::block::*;
+use super::production::ProductionBlock;
 
 make_simple!(
     DrillBlock,
@@ -23,7 +24,7 @@ make_register! {
     "blast-drill" => DrillBlock::new(4, true, cost!(Copper: 65, Titanium: 50, Thorium: 75, Silicon: 60));
     "water-extractor" => ExtractorBlock::new(2, true, cost!(Copper: 30, Lead: 30, Metaglass: 30, Graphite: 30));
     "oil-extractor" => ExtractorBlock::new(3, true, cost!(Copper: 150, Lead: 115, Graphite: 175, Thorium: 115, Silicon: 75));
-    "vent-condenser" => ExtractorBlock::new(3, true, cost!(Graphite: 20, Beryllium: 60));
+    "vent-condenser" => ProductionBlock::new(3, true, cost!(Graphite: 20, Beryllium: 60));
     "cliff-crusher" => WallCrafter::new(2, false, cost!(Beryllium: 100, Graphite: 40));
     "plasma-bore" => DrillBlock::new(2, false, cost!(Beryllium: 40));
     "large-plasma-bore" => DrillBlock::new(3, false, cost!(Silicon: 100, Oxide: 25, Beryllium: 100, Tungsten: 70));
